a797515cf3c925d74d838d9a8e92f32576d16b79
moisesdolores <62561953+moisesdolores@users.noreply.github.com>
2021-04-08 19:34:28 -0400

    Migrate posts (#15)
    * initializes node server and adds packages (#3)
    
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    
    * creates migrations for posts table
    
    Co-authored-by: weilyl <64621097+weilyl@users.noreply.github.com>
    Co-authored-by: Weily Lang <weily.lang@gmail.com>

---
+++ migrations/20210408231727_create_posts.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("posts", function (t) {
+    t.increments("id").unsigned().primary();
+    t.string("content").notNull();
+    t.timestamps(true, true);
+    t.integer("business_id").unsigned().notNullable();
+    t.foreign("business_id")
+      .references("id")
+      .inTable("businesses")
+      .onDelete("CASCADE");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("posts");
+};
