e95cdff3bf3b3d66ecafcff2dfaba90a2fff374d
weilyl <64621097+weilyl@users.noreply.github.com>
2021-04-08 16:19:59 -0400

    adds pg dependency and configures db.js (#12)
    Co-authored-by: Weily Lang <weily.lang@gmail.com>

---
+++ db.js
@@ -0,0 +1,13 @@
+const knexfile = require('./knexfile')
+
+const pg = require('pg-promise')()
+
+const db = pg({
+  "host": process.env.HOST,
+  "port": 5432,
+  "database": process.env.DATABASE,
+  "user": process.env.USER,
+  "password": process.env.PASSWORD
+})
+
+module.exports = db;
