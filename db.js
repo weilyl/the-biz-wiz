60262dd946ac33c8abd7c3e724e70a5d3c63cd57
Weily Lang <weily.lang@gmail.com>
2021-04-19 18:16:27 -0400

    reverts host name to heroku url and cleans it

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const knexfile = require("./knexfile");
 const pg = require("pg-promise")();
 
 const db = pg({
-  host: "ec2-34-233-0-64.compute-1.amazonaws.com",
+  host: "biz-wiz.herokuapp.com", //"ec2-34-233-0-64.compute-1.amazonaws.com",
   port: 5432,
   database: "d8uqu0bt7057ho",
   user: process.env.USER || "uikjstnhobxlmz",
