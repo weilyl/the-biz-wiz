87d443af1ef0b2b0a150f79340981b95c481a577
Weily Lang <weily.lang@gmail.com>
2021-04-19 19:11:39 -0400

    comments out ssl params and config in knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,17 +16,17 @@ module.exports = {
     client: "postgresql",
     connection:
       process.env.DATABASE_URL ||
-      "postgres://uikjstnhobxlmz:d47ca69623cdb10dc9875b877e9d59b274bded4b786f41a530c2bcaf43c30244@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d8uqu0bt7057ho?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
+      "postgres://uikjstnhobxlmz:d47ca69623cdb10dc9875b877e9d59b274bded4b786f41a530c2bcaf43c30244@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d8uqu0bt7057ho",
     pool: {
       min: 2,
       max: 10,
     },
-    ssl: false,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+    // ssl: false,
+    // extra: {
+    //   ssl: {
+    //     rejectUnauthorized: false,
+    //   },
+    // },
     migrations: {
       tableName: "knex_migrations",
     },
@@ -41,12 +41,12 @@ module.exports = {
       min: 2,
       max: 10,
     },
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: true,
-      },
-    },
+    // ssl: true,
+    // extra: {
+    //   ssl: {
+    //     rejectUnauthorized: true,
+    //   },
+    // },
     migrations: {
       tableName: "knex_migrations",
     },
