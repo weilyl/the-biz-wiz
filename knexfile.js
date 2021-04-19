8c314079e7407f81b8133efc493494796c04af11
Moises Dolores <moisesdolores96@gmail.com>
2021-04-19 15:56:24 -0400

    setting up deployment

--- knexfile.js
+++ knexfile.js
@@ -14,11 +14,8 @@ module.exports = {
 
   staging: {
     client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
+    connection:
+      "postgres://uikjstnhobxlmz:d47ca69623cdb10dc9875b877e9d59b274bded4b786f41a530c2bcaf43c30244@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d8uqu0bt7057ho",
     pool: {
       min: 2,
       max: 10,
