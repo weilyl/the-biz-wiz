68195393f56931a157845802bf8df25809cce451
Weily Lang <weily.lang@gmail.com>
2021-04-19 19:18:43 -0400

    cleans up code and removes security flaws

--- db.js
+++ db.js
@@ -3,33 +3,11 @@ const knexfile = require("./knexfile");
 const pg = require("pg-promise")();
 
 const db = pg({
-  host: "ec2-34-233-0-64.compute-1.amazonaws.com",
+  host: process.env.HOST,
   port: 5432,
-  database: "d8uqu0bt7057ho",
-  user: process.env.USER || "uikjstnhobxlmz",
-  password:
-    process.env.PASSWORD ||
-    "d47ca69623cdb10dc9875b877e9d59b274bded4b786f41a530c2bcaf43c30244",
-  // ssl: true,
-  // extra: {
-  //   ssl: {
-  //     rejectUnauthorized: true,
-  //   },
-  // },
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
 });
 
-/*
-const {Client} = require('pg');
-const params = {
-  host: 'ec2-34-233-0-64.compute-1.amazonaws.com',
-  user: 'uikjstnhobxlmz',
-  password: 'd47ca69623cdb10dc9875b877e9d59b274bded4b786f41a530c2bcaf43c30244',
-  port: 5432,
-  database: 'd8uqu0bt7057ho',
-  ssl: true
-};
-
-const client = new Client(params);
-client.connect();
-*/
 module.exports = db;
--- knexfile.js
+++ knexfile.js
@@ -15,18 +15,11 @@ module.exports = {
   staging: {
     client: "postgresql",
     connection:
-      process.env.DATABASE_URL ||
-      "postgres://uikjstnhobxlmz:d47ca69623cdb10dc9875b877e9d59b274bded4b786f41a530c2bcaf43c30244@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d8uqu0bt7057ho",
+      process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
     },
-    // ssl: false,
-    // extra: {
-    //   ssl: {
-    //     rejectUnauthorized: false,
-    //   },
-    // },
     migrations: {
       tableName: "knex_migrations",
     },
@@ -35,18 +28,11 @@ module.exports = {
   production: {
     client: "postgresql",
     connection:
-      process.env.DATABASE_URL ||
-      "postgres://uikjstnhobxlmz:d47ca69623cdb10dc9875b877e9d59b274bded4b786f41a530c2bcaf43c30244@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d8uqu0bt7057ho?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
+      process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
     },
-    // ssl: true,
-    // extra: {
-    //   ssl: {
-    //     rejectUnauthorized: true,
-    //   },
-    // },
     migrations: {
       tableName: "knex_migrations",
     },
