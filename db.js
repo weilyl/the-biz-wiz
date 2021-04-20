1541a00aedf554aff4addc844c4a4d6d588997f5
Weily Lang <weily.lang@gmail.com>
2021-04-20 14:03:02 -0400

    replaces pg-promise connection with database url

--- db.js
+++ db.js
@@ -3,11 +3,12 @@ const knexfile = require("./knexfile");
 const pg = require("pg-promise")();
 
 const db = pg({
-  host: process.env.HOST,
-  port: 5432,
-  database: process.env.DATABASE,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
+  // host: process.env.HOST,
+  // port: 5432,
+  // database: process.env.DATABASE,
+  // user: process.env.USER,
+  // password: process.env.PASSWORD,
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = db;
