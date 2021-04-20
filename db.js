3026a8cf3b84cb3cd0bec0e0a7a795eff65ce985
Weily Lang <weily.lang@gmail.com>
2021-04-20 14:11:42 -0400

    sets up development test config

--- db.js
+++ db.js
@@ -2,13 +2,14 @@ const knexfile = require("./knexfile");
 
 const pg = require("pg-promise")();
 
-const db = pg({
-  // host: process.env.HOST,
-  // port: 5432,
-  // database: process.env.DATABASE,
-  // user: process.env.USER,
-  // password: process.env.PASSWORD,
+const db = NODE_ENV === 'production' ? pg({
   connectionString: process.env.DATABASE_URL
+}) : pg({
+  host: process.env.test.HOST,
+  port: 5432,
+  database: process.env.test.DATABASE,
+  user: process.env.test.USER,
+  password: process.env.test.PASSWORD,
 });
 
 module.exports = db;
