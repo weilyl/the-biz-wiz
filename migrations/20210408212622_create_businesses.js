1e0abb6ed20c505c193a3a2aa1e101377f002897
Weily Lang <weily.lang@gmail.com>
2021-04-21 14:38:28 -0400

    changes double quotes to single quotes in migration files

--- migrations/20210408212622_create_businesses.js
+++ migrations/20210408212622_create_businesses.js
@@ -1,22 +1,22 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("businesses", function (t) {
-    t.increments("id").unsigned().primary();
-    t.string("business_name").notNull();
-    t.string("user_name").notNull().unique();
-    t.string("email").notNull();
-    t.string("password").notNull();
-    t.string("first_name");
-    t.string("last_name");
-    t.string("street_address");
-    t.string("city");
-    t.string("state");
-    t.integer("zip");
-    t.string("business_type").notNull();
-    t.string("acct_type").notNull();
-    t.string("logo");
+  return knex.schema.createTable('businesses', function (t) {
+    t.increments('id').unsigned().primary();
+    t.string('business_name').notNull();
+    t.string('user_name').notNull().unique();
+    t.string('email').notNull();
+    t.string('password').notNull();
+    t.string('first_name');
+    t.string('last_name');
+    t.string('street_address');
+    t.string('city');
+    t.string('state');
+    t.integer('zip');
+    t.string('business_type').notNull();
+    t.string('acct_type').notNull();
+    t.string('logo');
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("businesses");
+  return knex.schema.dropTable('businesses');
 };
--- migrations/20210409002912_create_comments.js
+++ migrations/20210409002912_create_comments.js
@@ -1,18 +1,18 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("comments", function (t) {
-    t.increments("id").unsigned().primary();
-    t.string("content").notNull();
+  return knex.schema.createTable('comments', function (t) {
+    t.increments('id').unsigned().primary();
+    t.string('content').notNull();
     t.timestamp(true, true);
-    t.integer("post_id").unsigned().notNullable();
-    t.integer("business_id").unsigned().notNullable();
-    t.foreign("post_id").references("id").inTable("posts").onDelete("CASCADE");
-    t.foreign("business_id")
-      .references("id")
-      .inTable("businesses")
-      .onDelete("CASCADE");
+    t.integer('post_id').unsigned().notNullable();
+    t.integer('business_id').unsigned().notNullable();
+    t.foreign('post_id').references("id").inTable("posts").onDelete("CASCADE");
+    t.foreign('business_id')
+      .references('id')
+      .inTable('businesses')
+      .onDelete('CASCADE');
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("comments");
+  return knex.schema.dropTable('comments');
 };
