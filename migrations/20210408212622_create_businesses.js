549a7892cdb1c1d8a72df18e33448046eeed8dc6
Weily Lang <weily.lang@gmail.com>
2021-04-21 11:34:33 -0400

    edits migration file to create business table

--- migrations/20210408212622_create_businesses.js
+++ migrations/20210408212622_create_businesses.js
@@ -3,11 +3,14 @@ exports.up = function (knex) {
     t.increments("id").unsigned().primary();
     t.string("business_name").notNull();
     t.string("user_name").notNull().unique();
+    t.string("email").notNull();
     t.string("password").notNull();
-    t.string("street_address").notNull();
-    t.string("city").notNull();
-    t.string("state").notNull();
-    t.integer("zip").notNull();
+    t.string("first_name");
+    t.string("last_name");
+    t.string("street_address");
+    t.string("city");
+    t.string("state");
+    t.integer("zip");
     t.string("business_type").notNull();
     t.string("acct_type").notNull();
     t.string("logo");
