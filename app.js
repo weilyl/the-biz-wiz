fbbe0683ba837ec3e882a2511ab8cefd7f7b7ae0
Weily Lang <weily.lang@gmail.com>
2021-04-21 11:56:12 -0400

    adds changes from weilyl fork dev branch that disappeared

--- app.js
+++ app.js
@@ -3,12 +3,43 @@ const log = require("./middleware/log");
 const express = require("express");
 const app = express();
 const businessRouter = require("./routes/business");
-// const session = require('express-session');
+const cors = require("cors");
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const whiteList = [
+    "https://biz-wiz.herokuapp.com/",
+    "http://localhost:3030/",
+    "http://localhost:3000/",
+    "http://localhost:4000/"
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(
+        new Error("Not allowed by CORS; domain needs to be added to whitelist")
+      );
+    }
+  },
+};
+
 
 //middleware
+// NODE_ENV === "development" ? app.use(cors()) : app.use(cors(corsOptions));
+
+
 app.use(express.urlencoded());
 app.use(express.json());
 
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// })
+
+app.use(cors())
 // set up sessions
 // app.use(
 //   session({
@@ -20,5 +51,7 @@ app.use(express.json());
 
 // routers
 app.use("/business", businessRouter);
-
+app.get('/', async (req, res) => {
+    console.log("you are here", req.headers);
+});
 module.exports = app;
