a7b22ca1fa3dec404ca7d7079cab4957a28351ec
weilyl <64621097+weilyl@users.noreply.github.com>
2021-04-26 01:11:17 -0400

    Changes to full outer join (#160)
    * changes HTTP status code from 302 to 200 for search content
    
    * adds business info and edits delete comments
    
    * makes search content case insensitive
    
    * changes res to req
    
    * adds routes to check if logged in as comment or post author
    
    * changes response body to object
    
    * pull from org dev
    
    * edits get all comments on posts SQL query to include author details
    
    * edits get all comments on posts SQL query to include author details
    
    * changes to full outer join
    
    * removes table aliases
    
    Co-authored-by: Weily Lang <weily.lang@gmail.com>

--- controllers/Comments.js
+++ controllers/Comments.js
@@ -17,14 +17,17 @@ async function getPostComments(req, res) {
     //   "SELECT * FROM comments WHERE post_id=$1",
     //   post_id
     // );
+    console.log("comments")
 
     const commentsEnhanced = await db.any(
       `SELECT * 
-      FROM comments as c 
-      WHERE post_id=${post_id} 
-      JOIN businesses as b 
-      ON (c.business_id = b.id)`
+      FROM comments 
+      FULL OUTER JOIN businesses 
+      ON comments.business_id = businesses.id 
+      WHERE comments.post_id = ${post_id} `
     , post_id);
+
+    console.log(commentsEnhanced)  
 
     return res.status(200).json(commentsEnhanced);
   } catch (err) {
